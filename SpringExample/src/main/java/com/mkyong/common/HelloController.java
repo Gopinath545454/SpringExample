41baa76d9b94c2922487e8c6c1a5e0817df97f7b
GOPINATH <GOPINATH@LEANSWIFT>
2015-07-27 17:18:18 +0530

    added comments

--- SpringExample/src/main/java/com/mkyong/common/HelloController.java
+++ SpringExample/src/main/java/com/mkyong/common/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
 
+	//adding comments
 	private int c;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
